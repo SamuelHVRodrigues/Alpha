61606ae3994fc97e126b0952767e5304b484a6e3
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2021-12-07 09:22:57 -0300

    adicionando corrida

---
+++ 06-javascript/11-objeto/script.js
@@ -0,0 +1,71 @@
+function corrida(numeroVoltas) {
+    let velocidadePedro;
+    let velocidadeJuca;
+    let velocidadeEdna;
+
+    let vitoriasPedro = 0;
+    let vitoriasJuca = 0;
+    let vitoriasEdna = 0;
+    
+    let campeao;
+    
+    for(let i = 0; i < numeroVoltas; i++) {
+        velocidadePedro = Math.floor((Math.random() * 80) + 150) * 0.97;
+        velocidadeJuca = Math.floor((Math.random() * 140) + 120) * 0.95;
+        velocidadeEdna = Math.floor((Math.random() * 40) + 180) * 0.99;
+        
+        if(velocidadePedro > velocidadeJuca) {
+            if(velocidadePedro > velocidadeEdna) {
+                vitoriasPedro++;
+            }
+            else {
+                vitoriasEdna++;
+            }
+        } else {
+            if(velocidadeJuca > velocidadeEdna) {
+                vitoriasJuca++;
+            }
+            else {
+                vitoriasEdna++;
+            }
+        }
+        if(i == numeroVoltas - 1) {
+            if(vitoriasPedro > vitoriasJuca) {
+                if(vitoriasPedro > vitoriasEdna) {
+                    campeao = "Pedro";
+                }
+                else if(vitoriasPedro == vitoriasEdna) {
+                    i--;
+                } else {
+                    campeao = "Edna";
+                }
+            } else {
+                if(vitoriasJuca > vitoriasEdna) {
+                    campeao = "Juca";
+                }
+                else if(vitoriasJuca == vitoriasEdna) {
+                    i--;
+                } else {
+                    campeao = "Edna";
+                }
+            }
+        }
+    }
+
+    let main = document.querySelector(".container");
+    let vencedor = document.createElement("span");
+    vencedor.innerHTML = `${campeao} venceu a corrida!`;
+    main.appendChild(vencedor);
+    
+    let jog1 = document.createElement("span");
+    jog1.innerHTML = `Pedro: ${vitoriasPedro}`;
+    main.appendChild(jog1);
+    
+    let jog2 = document.createElement("span");
+    jog2.innerHTML = `Juca: ${vitoriasJuca}`;
+    main.appendChild(jog2);
+    
+    let jog3 = document.createElement("span");
+    jog3.innerHTML = `Edna: ${vitoriasEdna}`;
+    main.appendChild(jog3);
+}
